f8b4f4fc697a798c67aed7173a0bffd4bab3e165
EricDeRegter <ericderegter@gmail.com>
2014-12-05 12:20:41 +0100

    selectors gemaakt
    13 ich selector er in gezet

--- Papple/src/com/example/papple/MainMenu.java
+++ Papple/src/com/example/papple/MainMenu.java
@@ -15,25 +15,64 @@ public class MainMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
-		
-		Button newproject = (Button) findViewById(R.id.newproject);
-		newproject.setOnClickListener(new OnClickListener() {
+
+		// Button newproject = (Button) findViewById(R.id.newproject);
+		// newproject.setOnClickListener(new OnClickListener() {
+		// @Override
+		// public void onClick(View v)
+		// {
+		// Intent intent = new Intent(getApplicationContext(), Project.class);
+		// startActivity(intent);
+		// }
+		// });
+
+		// Button loadproject = (Button) findViewById(R.id.loadproject);
+		// loadproject.setOnClickListener(new OnClickListener() {
+		// @Override
+		// public void onClick(View v)
+		// {
+		// Intent intent = new Intent(getApplicationContext(), Gallery.class);
+		// startActivity(intent);
+		// }
+		// });
+
+		Button dertien = (Button) findViewById(R.id.btnDertien);
+		dertien.setOnClickListener(new OnClickListener() {
+
 			@Override
-			public void onClick(View v)
-			{
-				Intent intent = new Intent(getApplicationContext(), Project.class);
-				startActivity(intent);
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
 			}
 		});
 		
-		Button loadproject = (Button) findViewById(R.id.loadproject);
-		loadproject.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v)
-			{
-				Intent intent = new Intent(getApplicationContext(), Gallery.class);
-				startActivity(intent);
-			}
-		});
+//		Button vijftien = (Button) findViewById(R.id.btnVijftien);
+//		vijftien.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//		});
+//
+//		Button openProject = (Button) findViewById(R.id.btnOpenProject);
+//		openProject.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//		});
+//
+//		Button newProject = (Button) findViewById(R.id.btnNewProject);
+//		newProject.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//
+//			}
+//		});
 	}
 }
