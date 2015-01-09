79fb52a91483fc07867d639d55ff404063fe6a2b
EricDeRegter <ericderegter@gmail.com>
2015-01-09 09:15:12 +0100

    app aangepast zodat deze te runnen was via mijn pc

--- PocResizeImg/src/com/poc/pocresizeimg/MainActivity.java
+++ PocResizeImg/src/com/poc/pocresizeimg/MainActivity.java
@@ -1,6 +1,6 @@
 package com.poc.pocresizeimg;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
@@ -15,7 +15,7 @@ import android.widget.Button;
 import android.widget.ListView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	private static final int RESULT_LOAD_IMAGE = 1;
 	private layerAdapter adapter;
@@ -65,25 +65,5 @@ public class MainActivity extends ActionBarActivity {
     		cd.invalidate();
     		cd.resetMatrix();
     	}
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
