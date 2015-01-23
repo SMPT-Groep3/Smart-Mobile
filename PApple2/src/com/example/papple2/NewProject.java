c75622a281e8c95e7f602fed9004560e9d4a05bb
EricDeRegter <ericderegter@gmail.com>
2015-01-23 19:04:07 +0100

    plaatsjes kleiner
    Alleen volgorde nog aanpassen

--- PApple2/src/com/example/papple2/NewProject.java
+++ PApple2/src/com/example/papple2/NewProject.java
@@ -31,13 +31,13 @@ public class NewProject extends Activity {
 		List<ItemPart> items = itemProvider.getItems();
 		if(items.size() == 0)
 		{
-			ItemPart item1 = new ItemPart("Cover", getResources().getDrawable(R.drawable.ic_launcher), getResources().getColor(color.rood), getResources().getDrawable(R.drawable.coverd));
-			ItemPart item2 = new ItemPart("Shell", getResources().getDrawable(R.drawable.ic_launcher), getResources().getColor(color.paars), getResources().getDrawable(R.drawable.shelld));
-			ItemPart item3 = new ItemPart("Screen", getResources().getDrawable(R.drawable.ic_launcher), getResources().getColor(color.grijs), getResources().getDrawable(R.drawable.screend));
-			ItemPart item4 = new ItemPart("Side", getResources().getDrawable(R.drawable.ic_launcher), getResources().getColor(color.blauw), getResources().getDrawable(R.drawable.sided));
-			ItemPart item5 = new ItemPart("Keyboard", getResources().getDrawable(R.drawable.keyboard), getResources().getColor(color.rood), getResources().getDrawable(R.drawable.keyboardd));
-			ItemPart item6 = new ItemPart("Trackpad", getResources().getDrawable(R.drawable.touchpad), getResources().getColor(color.paars), getResources().getDrawable(R.drawable.trackpadd));
-			ItemPart item7 = new ItemPart("Adapter", getResources().getDrawable(R.drawable.adapter), getResources().getColor(color.grijs), getResources().getDrawable(R.drawable.adapterd));
+			ItemPart item1 = new ItemPart("Keyboard", getResources().getDrawable(R.drawable.keyboard), getResources().getColor(color.rood), getResources().getDrawable(R.drawable.keyboardd));
+			ItemPart item2 = new ItemPart("Trackpad", getResources().getDrawable(R.drawable.touchpad), getResources().getColor(color.paars), getResources().getDrawable(R.drawable.trackpadd));
+			ItemPart item3 = new ItemPart("Adapter", getResources().getDrawable(R.drawable.adapter), getResources().getColor(color.grijs), getResources().getDrawable(R.drawable.adapterd));
+			ItemPart item4 = new ItemPart("Cover", getResources().getDrawable(R.drawable.shell), getResources().getColor(color.rood), getResources().getDrawable(R.drawable.coverd));
+			ItemPart item5 = new ItemPart("Shell", getResources().getDrawable(R.drawable.ic_launcher), getResources().getColor(color.paars), getResources().getDrawable(R.drawable.shelld));
+			ItemPart item6 = new ItemPart("Screen", getResources().getDrawable(R.drawable.ic_launcher), getResources().getColor(color.grijs), getResources().getDrawable(R.drawable.screend));
+			ItemPart item7 = new ItemPart("Side", getResources().getDrawable(R.drawable.ic_launcher), getResources().getColor(color.blauw), getResources().getDrawable(R.drawable.sided));
 			ItemPart item8 = new ItemPart("Onderkant", getResources().getDrawable(R.drawable.ic_launcher), getResources().getColor(color.blauw), getResources().getDrawable(R.drawable.bottomd));
 			
 			itemProvider.addItemPart(item1);
